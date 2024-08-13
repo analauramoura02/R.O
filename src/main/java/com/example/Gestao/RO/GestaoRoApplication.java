39c29a287de9841db8e431403ec40137dbe8f578
kamila1917 <kamilabalduinocosta7648@gmail.com>
2024-08-13 08:39:50 -0300

    Commit da estrutura inicial do projeto TCC

---
+++ src/main/java/com/example/Gestao/RO/GestaoRoApplication.java
@@ -0,0 +1,13 @@
+package com.example.Gestao.RO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoRoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoRoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Gestao/RO/GestaoRoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Gestao.RO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoRoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
