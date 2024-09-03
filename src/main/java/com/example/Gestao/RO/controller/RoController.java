22c9371a6ee031e220ec91f40afc9aaecb6c8a3d
Ana Laura <analauraxx77@gmail.com>
2024-09-03 13:47:10 -0300

    Criação das classes e formularios do cadastro de Ros

--- src/main/java/com/example/Gestao/RO/controller/RoController.java
+++ src/main/java/com/example/Gestao/RO/controller/RoController.java
@@ -1,20 +1,26 @@
 package com.example.Gestao.RO.controller;
 
-import com.example.Gestao.RO.model.Aluno;
+import com.example.Gestao.RO.model.Ro;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/aluno")
-public class AlunoController {
+@RequestMapping("/ro")
+public class RoController {
+
+    @GetMapping
+    public String listagem(Model model){
+
+        return "ro/listagem";
+    }
 
     @GetMapping("/form-inserir")
     public String formInserir(Model model){
-        model.addAttribute("aluno", new Aluno());
-        return "aluno/form-inserir";
-    }
+        model.addAttribute("ro", new Ro());
 
+        return "ro/form-inserir";
+    }
 
 }
